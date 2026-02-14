9a7d7e04deabfd27055684be5beb009e7b272f93
Jaidev <jvk@durvah.com>
2026-02-14 16:37:33 +0530

    vite@frontend-new removed

--- frontendReact/src/index.js
+++ frontendReact/src/index.js
@@ -5,10 +5,11 @@ import App from './App'
 import './index.css'
 import './style.css' // Importing original styles
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
